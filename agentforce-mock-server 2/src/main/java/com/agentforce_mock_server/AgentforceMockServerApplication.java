1f12fd08a24ea38d814b67538d82a4174e0a7f13
tarunkumar938 <101572878+tarunkumar938@users.noreply.github.com>
2025-04-03 16:36:06 +0530

    adding boilerplate code

---
+++ agentforce-mock-server 2/src/main/java/com/agentforce_mock_server/AgentforceMockServerApplication.java
@@ -0,0 +1,13 @@
+package com.agentforce_mock_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentforceMockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentforceMockServerApplication.class, args);
+	}
+
+}
---
+++ agentforce-mock-server 2/src/test/java/com/agentforce_mock_server/AgentforceMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentforce_mock_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentforceMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
