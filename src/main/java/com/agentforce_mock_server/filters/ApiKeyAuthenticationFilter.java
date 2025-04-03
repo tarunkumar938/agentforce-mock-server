a0fd0adb88ce0d601a9f47af99f92b1aea57a251
teja <nagamohanp@backflipt.com>
2025-04-03 19:36:43 +0530

    adding web filter for api key authentication

---
+++ src/main/java/com/agentforce_mock_server/filters/ApiKeyAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.agentforce_mock_server.filters;
+
+import com.agentforce_mock_server.utils.AgentForceConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class ApiKeyAuthenticationFilter implements WebFilter {
+
+    @Value("${apikey}")
+    private String VALID_API_KEY;
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String apiKey = exchange.getRequest().getHeaders().getFirst(AgentForceConstants.API_KEY_HEADER);
+
+        if (apiKey == null || !apiKey.equals(VALID_API_KEY)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
---
+++ src/main/java/com/agentforce_mock_server/utils/AgentForceConstants.java
@@ -0,0 +1,10 @@
+package com.agentforce_mock_server.utils;
+
+import lombok.Data;
+
+@Data
+public class AgentForceConstants {
+
+    public static final String API_KEY_HEADER = "X-Api-key";
+
+}
